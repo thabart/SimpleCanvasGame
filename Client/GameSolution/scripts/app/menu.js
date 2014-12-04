c3ed260426cedbdfc7cfa2609bbedf8a2858bff4
Thierry Habart <habarthierry@hotmail.fr>
2014-12-04 19:45:29 +0100

    Start the animation for Button

--- Client/GameSolution/scripts/app/menu.js
+++ Client/GameSolution/scripts/app/menu.js
@@ -1,11 +1,21 @@
 ﻿define([
    "app/SpriteSheet/ActionSpriteSheet",
    "app/SpriteSheetLoader",
+    "easel",
+    "preloadjs"
 ], function (spriteSheet, spriteSheetLoader) {
 
     var defaultState = "init";
     var spriteData = spriteSheet;
 
+    var MyGeometry = {
+        mixScaleAndRotationX: function (scale, rotation) {
+            return Math.sin(rotation / 360 * Math.PI) * scale;
+        },
+        mixScaleAndRotationY: function (scale, rotation) {
+            return Math.cos(rotation / 360 * Math.PI) * scale;
+        }
+    }
 
     var sprite = spriteSheet;
 
@@ -14,12 +24,23 @@
             var spriteSheetId = spriteData.id;
             var spriteSheet = spriteSheetLoader.GetSpriteSheet(spriteSheetId);
             var animation = new createjs.Sprite(spriteSheet, defaultState);
+            
+            var container = stage.addChild(animation);
+            container.x = 20;
+            container.y = 30;
+            container.rotationX = 0;
+            container.rotationY = 0;
+            container.rscaleY = 1;
 
-            animation.x = 0;
-            animation.y = 0;
-            animation.skewY = 20;
-            stage.addChild(animation);
+            function tmp() {
+                container.rotationY += 4;
+                // container.scaleX = MyGeometry.mixScaleAndRotationX(container.rscaleX, container.rotationX + 90);
+                container.scaleY = MyGeometry.mixScaleAndRotationY(container.rscaleY, container.rotationY + 90);
+                stage.update();
+            }
+
             stage.update();
+            createjs.Ticker.addEventListener("tick", tmp);
         }
     };
 });
